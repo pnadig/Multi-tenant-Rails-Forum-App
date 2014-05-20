2b2a6aea596c7fc61d4086b772a6f5a240429bda
Omar Ismail <oismail91@gmail.com>
2014-05-20 07:41:35 -0400

    built subdomain authentication

--- subscribem/app/models/subscribem/account.rb
+++ subscribem/app/models/subscribem/account.rb
@@ -3,6 +3,8 @@ module Subscribem
     EXCLUDED_SUBDOMAINS = %w(admin) 
     
     belongs_to :owner, :class_name => "Subscribem::User"
+    has_many :members, :class_name => "Subscribem::Member"
+    has_many :users, :through => :members
     accepts_nested_attributes_for :owner
     
     before_validation do 
@@ -11,5 +13,13 @@ module Subscribem
     validates_format_of :subdomain, :with => /\A[\w\-]+\Z/i, :message => "is not allowed. Please choose another subdomain."
     validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS, :message => "is not allowed. Please choose another subdomain."
     validates :subdomain, :presence => true, :uniqueness => true
+    
+    def self.create_with_owner(params = {})
+      account = new(params)
+      if account.save
+        account.users << account.owner
+      end
+      account
+    end 
   end
 end
--- subscribem/spec/models/subscribem/account_spec.rb
+++ subscribem/spec/models/subscribem/account_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 module Subscribem
   describe Account do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "can be created with an owner" do 
+      params = {
+        :name => "Test Account",
+        :subdomain => "test",
+        :owner_attributes => {
+          :email => "user@example.com",
+          :password => "password",
+          :password_confirmation => "password"
+        }
+      }
+
+      account = Subscribem::Account.create_with_owner(params)
+      expect(account).to be_persisted
+      expect(account.users.first).to eq(account.owner)
+    end
+
+    it "cannot create an account without a subdomain" do 
+      account = Subscribem::Account.create_with_owner
+      expect(account).to_not be_valid
+      expect(account.users).to be_empty
+    end
   end
 end
